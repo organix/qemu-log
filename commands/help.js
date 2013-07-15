e93f3fa62e5ec8b5f6e38a20e42095880ff26a04
Tristan Slominski <tristan.slominski@gmail.com>
2013-07-15 14:04:33 -0500

    qemu-log lines emits line when enountering '\n' as first character in buffer

--- commands/help.js
+++ commands/help.js
@@ -30,14 +30,14 @@ THE SOFTWARE.
 var clie = require('clie');
 
 var help = module.exports = clie.command(function (args) {
-	help.data(help.usage).end();
+    help.data(help.usage).end();
 });
 
 help.usage = [
-	"\nUsage: qemu-log <command>",
-	"",
-	"where <command> is one of:",
-	"    help, lines, parse, top-blocks",
-	"",
-	"qemu-log <command> -h   quick help on <command>"
+    "\nUsage: qemu-log <command>",
+    "",
+    "where <command> is one of:",
+    "    help, lines, parse, top-blocks",
+    "",
+    "qemu-log <command> -h   quick help on <command>"
 ].join('\n');
